2e9678311bfe747d8a313331e179ac3303762886
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-04 19:02:55 +0300

    Delete server.lua

--- server.lua
+++
@@ -1,6 +0,0 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
-QBCore.Functions.CreateUseableItem(Config.WheelchairItem, function(source, item)
-    local Player = QBCore.Functions.GetPlayer(source)
-    TriggerClientEvent('QBCore:Client:OnUseItem', source, item.name)
-end)
