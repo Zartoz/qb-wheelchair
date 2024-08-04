868f1b4d80635c9f529a64b0e2c8e1483a312779
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-04 17:47:22 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Register the item
 QBCore.Functions.CreateUseableItem(Config.WheelchairItem, function(source, item)
     local Player = QBCore.Functions.GetPlayer(source)
     TriggerClientEvent('QBCore:Client:OnUseItem', source, item.name)
