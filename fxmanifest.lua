5534e71673e588afb62aed5e58beab92f2c21a1b
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-04 17:49:41 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Zartoz'
-description 'Wheelchair Script'
+description 'A simple wheelchair script made for QBCore Framework'
 version '1.0.0'
 
 shared_scripts {
