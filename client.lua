ec3d38d9bdcec8d1b5af65ae22a27836f038c6eb
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-04 17:47:47 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,6 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Function to spawn the wheelchair
 local function spawnWheelchair(playerPed)
     local playerCoords = GetEntityCoords(playerPed)
     local spawnCoords = vector3(playerCoords.x + 2, playerCoords.y, playerCoords.z)
@@ -11,14 +10,11 @@ local function spawnWheelchair(playerPed)
         Wait(100)
     end
 
-    -- Create the vehicle
     local vehicle = CreateVehicle('iak_wheelchair', spawnCoords.x, spawnCoords.y, spawnCoords.z, GetEntityHeading(playerPed), true, false)
     
-    -- Set the player in the vehicle
     TaskWarpPedIntoVehicle(playerPed, vehicle, -1)
 end
 
--- Event handler for item use
 RegisterNetEvent('QBCore:Client:OnUseItem')
 AddEventHandler('QBCore:Client:OnUseItem', function(itemName)
     if itemName == Config.WheelchairItem then
