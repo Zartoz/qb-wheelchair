4f51ea1dab07937e42380185816cda1388d4b052
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-05 23:11:45 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,3 +3,4 @@ Config = {}
 Config.WheelchairItem = 'wheelchair' -- Wheelchair item
 Config.WheelchairModel = 'iak_wheelchair' -- Vehicle spawn code 
 Config.TargetSystem = 'ox_target'  -- Target system currenly supported only qb-target and ox_target
+Config.FuelScript = 'cdn-fuel' -- Fuel system currently supported only ps-fuel, cdn-fuel and LegacyFuel
